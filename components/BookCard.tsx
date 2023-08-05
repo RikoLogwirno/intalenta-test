b6c2fcee1cd57d84b7ec2d1ab820666e153d9383
Riko Logwirno <logwirno.teknus@gmail.com>
2023-08-06 00:37:42 +0700

    feat(component): Layout BookCard component

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,5 +1,35 @@
-export default function BookCard() {
+import Image from "next/image";
+
+import { HeartCircle, HeartCircleOutline } from "@/assets";
+
+import Card from "./Card";
+
+type BookCardTypes = {
+  id: number;
+  title: string;
+  author: string;
+  description: string;
+  cover: string;
+  publicationDate: string;
+};
+
+export default function BookCard(props: BookCardTypes) {
   return (
-    <div>BookCard</div>
+    <Card>
+      <div className="book-card">
+        <div className="cover">
+          <img src={ props.cover } alt={ props.title } />
+        </div>
+        <div className="descriptions">
+          <p className="font-l">{ props.title }</p>
+          <p className="font-m">{ props.description }</p>
+          <p className="font-s">{ props.author }</p>
+          <p className="font-s">{ props.publicationDate }</p>
+          <div>
+            <Image src={ HeartCircle } alt="HeartCircle" />
+          </div>
+        </div>
+      </div>
+    </Card>
   );
 }
