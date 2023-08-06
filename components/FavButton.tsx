c310d7eb32a80eb045812447c5c1b45be94f4c6d
Riko Logwirno <logwirno.teknus@gmail.com>
2023-08-06 16:51:25 +0700

    feat(component): Add onClick event

--- components/FavButton.tsx
+++ components/FavButton.tsx
@@ -5,7 +5,7 @@ import { HeartCircle, HeartCircleOutline } from "@/assets";
 
 export default function FavButton(props: FavButtonTypes) {
   return (
-    <div className="fav-button">
+    <div className="fav-button" onClick={ props.onClick }>
       <Image src={ props.status ? HeartCircle : HeartCircleOutline } alt="Fav button" />
     </div>
   );
