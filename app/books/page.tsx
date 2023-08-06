f2ba75ff6cc8b694c2285553e9a0e7350b387ee6
Riko Logwirno <logwirno.teknus@gmail.com>
2023-08-06 16:50:27 +0700

    feat(component): Add books page

---
+++ app/books/page.tsx
@@ -0,0 +1,17 @@
+import BookCard from "@/components/BookCard";
+
+const randomDesc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In purus nisi, lobortis vitae turpis nec, suscipit luctus felis. Sed eleifend eleifend magna eget iaculis. Ut nulla lacus, commodo eget sapien eget, vestibulum scelerisque erat. Sed id ipsum eu purus fermentum iaculis. Nullam a arcu nec velit ultrices consectetur ac nec odio. Sed non mollis dui, sed auctor nisl. Donec interdum maximus mauris, pulvinar eleifend dolor aliquet eu. Vivamus dignissim, sem sed imperdiet mollis, metus sapien auctor mauris, ut tempor sapien ligula sed dolor. Quisque dolor leo, malesuada quis ante vel, tempor efficitur leo. Sed nec imperdiet neque. Curabitur at malesuada ex. Nunc eu feugiat arcu, eget gravida massa. Fusce tempus urna magna, eleifend posuere lorem vehicula ac. In vitae hendrerit diam. Praesent at mi nisi. Etiam ac mattis sapien. Nam elementum velit sapien, at laoreet justo convallis et. Morbi non mollis nibh. In efficitur sapien laoreet euismod laoreet. Aenean aliquet est ex, vel auctor metus venenatis id. Vestibulum consectetur erat lacinia viverra euismod. Mauris elementum sapien eu ultrices convallis. Cras nec augue vel justo ornare pulvinar vitae a odio. Morbi et enim nunc. Sed quis lorem ultricies, ullamcorper libero vel, rutrum magna. In consequat justo neque, sed consequat orci tempor sit amet. Donec nibh arcu, bibendum id nunc ac, venenatis rutrum augue. Etiam quam quam, hendrerit at sapien vel, ultricies aliquam ipsum. Aenean eleifend ipsum nec semper efficitur. In varius nibh dui, quis sollicitudin dolor iaculis eget.";
+
+export default function books() {
+  return (
+    <main className="standard-page books">
+      <section>
+        <div style={ { display: 'flex', flexDirection: 'column', gap: '10px' } }>
+          <BookCard id={ 1 } title="Lorem" description={ randomDesc } author="Riko Logwirno" publicationDate="1925-04-10T00:00:00.000Z" cover="https://m.media-amazon.com/images/I/41NssxNlPlS._SY291_BO1,204,203,200_QL40_FMwebp_.jpg" />
+          <BookCard id={ 1 } title="Lorem" description={ randomDesc } author="Riko Logwirno" publicationDate="1925-04-10T00:00:00.000Z" cover="https://m.media-amazon.com/images/I/41NssxNlPlS._SY291_BO1,204,203,200_QL40_FMwebp_.jpg" />
+          <BookCard id={ 1 } title="Lorem" description={ randomDesc } author="Riko Logwirno" publicationDate="1925-04-10T00:00:00.000Z" cover="https://m.media-amazon.com/images/I/41NssxNlPlS._SY291_BO1,204,203,200_QL40_FMwebp_.jpg" />
+        </div>
+      </section>
+    </main>
+  );
+}
